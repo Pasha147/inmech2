2cdc454939cc4a05fa38b14bf3740ee91e04c4ce
Pasha147 <pavel147223@gmail.com>
2024-01-22 10:07:13 +0200

    Header burger has completed

--- app/ui/header_nav.tsx
+++ app/ui/header_nav.tsx
@@ -5,8 +5,11 @@ import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import clases from '@/app/ui/header_nav.module.css'
 import Image from 'next/image';
+import { useState } from 'react';
 
 export default function HeaderNav() {
+
+    const [burMenu, setBurMenu] = useState(false);
     const pathname = usePathname();
     const links = [
         {
@@ -35,6 +38,11 @@ export default function HeaderNav() {
         },
     ];
 
+    function hanndleClick() {
+        burMenu ? setBurMenu(false) : setBurMenu(true);
+        // console.log('burgerMenu = ', burMenu);
+    }
+
     return (
         <header className={clases.header}>
             <div className='container'>
@@ -42,12 +50,12 @@ export default function HeaderNav() {
                     <Link
                         href="/"
                         className={clases.link__logo}
-                       
+
                     >
                         <Image
                             src="/img/logo5.png"
                             width={300}
-                            height= {92}
+                            height={92}
                             priority={true}
                             className={clases.logoImg}
                             // style={{
@@ -59,14 +67,26 @@ export default function HeaderNav() {
                         />
                         {/* <img className="logo" src="./img/logo5.png" alt="Logo"></img> */}
                     </Link>
-                    <nav className={clases.nav}>
+                    <div
+                        className={
+                            burMenu ?
+                                [clases.header__burger, clases.header__burgerOff].join(' ')
+                                :
+                                clases.header__burger
+                        }
+                        // onClick={hanndleClick}
+                        onClick={() => setBurMenu(prev => !prev)}
+                    >
+                        <span></span>
+                    </div>
+                    <nav className={burMenu ? clases.nav : [clases.nav, clases.navOff].join(' ')}>
                         {links.map((link) => {
                             return (
                                 <Link
                                     key={link.name}
                                     href={link.href}
                                     className={`${clases.navLink} ${pathname === link.href && clases.navLinkActive}`}
-
+                                    onClick={() => setBurMenu(prev => !prev)}
                                 >
                                     {link.name}
                                 </Link>
