3c59f6225cd5a8340d38c806728ea77a858cd4b4
Pasha147 <pavel147223@gmail.com>
2024-02-08 14:11:41 +0200

    Second Edit button has been added B

--- app/ui/editCurNews.tsx
+++ app/ui/editCurNews.tsx
@@ -4,9 +4,12 @@
 import { useEffect, useState } from "react"
 import { INewsMassage } from "../lib/definitions"
 import EditForm from "./editForm";
+import classes from './editForm.module.css'
+import { editNews } from "@/app/lib/action";
 
 export default function EditCurNews ({curNews}:{curNews:INewsMassage}){
     const [isForm, setIsForm]=useState(false)
+    const updateNewsWithId = editNews.bind(null, curNews.id);
     
     return (
         <>
@@ -15,7 +18,75 @@ export default function EditCurNews ({curNews}:{curNews:INewsMassage}){
                 onClick={()=>setIsForm(prev=>!prev)}
             >Edit news</button>
             {
-             isForm && <EditForm curNews={curNews}/>
+            //  isForm && <EditForm curNews={curNews}/>
+            isForm && (
+                <div className={classes.editForm}>
+                <form
+                     action={updateNewsWithId}
+                     // className={classes.editForm}
+                 >
+                     <h2 className={classes.h2}>Edit current news</h2>
+                     <label
+                         htmlFor="date"
+                         className={classes.label}
+                     >
+                         Date
+                     </label>
+                     <input
+                         id="date"
+                         name="date"
+                         type="text"
+                         placeholder="dd/mm/YYYY"
+                         className={classes.dateInp}
+                         defaultValue={curNews.date}
+                     />
+                     <label
+                         htmlFor="title"
+                         className={classes.label}
+                     >
+                         Title
+                     </label>
+                     <input
+                         id="title"
+                         name="title"
+                         type="text"
+                         placeholder="Title"
+                         className={classes.titleInp}
+                         defaultValue={curNews.title}
+                     />
+                     <label
+                         htmlFor="img"
+                         className={classes.label}
+                     >
+                         img
+                     </label>
+                     <input
+                         id="img"
+                         name="img"
+                         type="text"
+                         placeholder="img"
+                         className={classes.titleInp}
+                         defaultValue={curNews.img}
+                     />
+                     <label
+                         htmlFor="text"
+                         className={classes.label}
+                     >
+                         Text
+                     </label>
+                     <textarea
+                         id="text"
+                         name="text"
+                         className={classes.textArea}
+                         placeholder="Text"
+                         defaultValue={curNews.text}
+                     ></textarea>
+                     <button
+                         type="submit"
+                         className={classes.submitBtn}>Seve news</button>
+                 </form>
+             </div>
+            )
 
             }
         </>
